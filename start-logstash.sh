7c40e918086a0c971c89279da458e16439fda5e5
Andreas Elvers <andreas.githubcontribution@gomorrha.de>
2014-10-19 18:41:02 +0200

    FIX: logstash now parses all logfiles and syslog

--- start-logstash.sh
+++ start-logstash.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo 'cat <<END_OF_TEXT' >  temp.sh
-cat /docker-elk/logstash.d/logstash.conf  >> temp.sh
-echo 'END_OF_TEXT'       >> temp.sh
-bash temp.sh > /docker-elk/logstash.d/logstash.conf
-rm temp.sh
-
 cd /logstash
 exec /logstash/bin/logstash -f /docker-elk/logstash.d/logstash.conf
